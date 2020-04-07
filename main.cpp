3d051fcefea4f4bf89f95708d46a50ef910000bb
Bence Bako <bakobence19@gmail.com>
2020-04-07 18:18:26 +0200

    some changes

--- main.cpp
+++ main.cpp
@@ -10,4 +10,11 @@ int main(){
         if(a(0, 0)!=1 || a(0, 1)!=2 || a(1,0) != 3 || a(1,1)!=4) { cout << "Error in indexing ()" << endl;}
         if(a[0]!=1 || a[1]!=2 || a[2] != 3 || a[3]!=4) { cout << "Error in indexing []" << endl;}
     }
+    //Test addition operator
+    {
+        Matrix<double> A={2}; Matrix<double> B={2};
+        A.data = {1, 2, 3, 4}; B.data = {1, 2, 3, 4};
+        Matrix<double> R = A+B;
+        if(R(0,0)!=2 || R(0, 1)!=4 || R(1,0)!=6 || R(1,1)!=8) {cout << "problem in addition" << endl;}
+    }
 }
--- matrix.h
+++ matrix.h
@@ -21,3 +21,25 @@ struct Matrix
     T& operator[](int i) {return data[i];}
     T const& operator[] (int i) const {return data[i]; }
 };
+
+//Addition operators 4 versions for all combinations of const& and &&:
+template <typename T>
+Matrix<T> operator+(Matrix<T> const& A, Matrix<T> const& B){
+    Matrix<T> result; result.data.resize(A.N);
+    result.N = A.N;
+    for(int i=0; i<A.N; i++){
+        for(int j=0; j<A.N; j++)
+            result.data[i*result.N+j]=A.data[i*A.N+j]+B.data[i*A.N+j];
+    }
+    return result;
+}
+template <typename T>
+Matrix<T>&& operator+(Matrix<T> && A, Matrix<T> && B){
+    Matrix<T> result; result.data.resize(A.N);
+    result.N = A.N;
+    for(int i=0; i<A.N; i++){
+        for(int j=0; j<A.N; j++)
+            result.data[i*result.N+j]=A.data[i*A.N+j]+B.data[i*A.N+j];
+    }
+    return result;
+}
